3dcc915de4b38785824d555e50275f960f721320
Cameron Clough <cameronjclough@gmail.com>
2025-02-05 01:03:14 +0000

    cleanup(usblib): separate connect and validate methods (#34)

--- src/usblib.js
+++ src/usblib.js
@@ -1,98 +1,99 @@
 import { concatUint8Array, sleep } from "./utils";
 
-const vendorId = 0x05c6;
-const productId = 0x9008;
-const QDL_USB_CLASS = 0xff;
+
+/**
+ * @type {USBDeviceFilter}
+ */
+const QDL_DEVICE_FILTER = {
+  vendorId: 0x05c6, productId: 0x9008, classCode: 0xff,
+};
+
+
 const BULK_TRANSFER_SIZE = 16384;
 
 
 export class usbClass {
   constructor() {
+    /** @type {USBDevice|null} */
     this.device = null;
+    /** @type {USBEndpoint|null} */
     this.epIn = null;
+    /** @type {USBEndpoint|null} */
     this.epOut = null;
     this.maxSize = 512;
   }
 
   get connected() {
-    return (
-      this.device?.opened &&
-      this.device.configurations[0].interfaces[0].claimed
-    );
+    return this.device?.opened && this.device.configurations[0].interfaces[0].claimed;
   }
 
-  async _validateAndConnectDevice() {
-    let ife = this.device?.configurations[0].interfaces[0].alternates[0];
+  /**
+   * @param {USBDevice} device
+   * @returns {{ epIn: USBEndpoint, epOut: USBEndpoint }}
+   */
+  #validateDevice(device) {
+    const ife = device.configurations[0].interfaces[0].alternates[0];
     if (ife.endpoints.length !== 2) {
       throw "USB - Attempted to connect to null device";
     }
-
-    this.epIn = null;
-    this.epOut = null;
-
-    for (let endpoint of ife.endpoints) {
+    let epIn = null, epOut = null;
+    for (const endpoint of ife.endpoints) {
       if (endpoint.type !== "bulk") {
         throw "USB - Interface endpoint is not bulk";
       }
       if (endpoint.direction === "in") {
-        if (this.epIn === null) {
-          this.epIn = endpoint;
-        } else {
+        if (epIn) {
           throw "USB - Interface has multiple IN endpoints";
         }
+        epIn = endpoint;
       } else if (endpoint.direction === "out") {
-        if (this.epOut === null) {
-          this.epOut = endpoint;
-        } else {
+        if (epOut) {
           throw "USB - Interface has multiple OUT endpoints";
         }
+        epOut = endpoint;
       }
     }
-    console.log("Endpoints: in =", this.epIn, ", out =", this.epOut);
+    console.debug("[usblib] endpoints: in =", epIn, ", out =", epOut);
+    this.epIn = epIn;
+    this.epOut = epOut;
+    this.maxSize = this.epIn.packetSize;
+  }
 
-    if (this.epIn) {
-      this.maxSize = this.epIn.packetSize;
-    }
-
+  /**
+   * @param {USBDevice} device
+   * @returns {Promise<void>}
+   * @private
+   */
+  async #connectDevice(device) {
+    this.device = device;
+    this.#validateDevice(device);
     try {
-      await this.device?.open();
-      await this.device?.selectConfiguration(1);
-      try {
-        await this.device?.claimInterface(0);
-      } catch(error) {
-        await this.device?.reset();
-        await this.device?.forget();
-        await this.device?.close();
-        console.error(error);
-      }
+      await device.open();
+      await device.selectConfiguration(1);
+      await device.claimInterface(0);
     } catch (error) {
-      throw `USB - ${error}`;
+      try {
+        await device.reset();
+        await device.forget();
+        await device.close();
+      } catch {
+        // ignore cleanup errors
+      }
+      throw new Error("Error while connecting to device", { cause: error });
     }
   }
 
   async connect() {
-    this.device = await navigator.usb.requestDevice({
-      filters: [
-        {
-          vendorId,
-          productId,
-          classCode: QDL_USB_CLASS,
-        },
-      ],
+    const device = await navigator.usb.requestDevice({
+      filters: [QDL_DEVICE_FILTER],
     });
-    console.log("Using USB device:", this.device);
-
-    navigator.usb.addEventListener("connect", async (event) =>{
-      console.log("USB device connect:", event.device);
-      this.device = event.device;
-      try {
-        await this._validateAndConnectDevice();
-      } catch (error) {
-        console.log("Error while connecting to the device");
-        throw error;
-      }
+    console.debug("[usblib] Using USB device:", device);
+    // TODO: is this event listener required?
+    navigator.usb.addEventListener("connect", async (event) => {
+      console.debug("[usblib] USB device connected:", event.device);
+      await this.#connectDevice(event.device);
     });
-    await this._validateAndConnectDevice();
+    await this.#connectDevice(device);
   }
 
   /**
