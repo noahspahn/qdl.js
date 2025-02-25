98f7dd89f8fc3da0c4e66690fd0db0d94e5ee669
Cameron Clough <cameronjclough@gmail.com>
2025-02-25 23:43:02 +0000

    test: use android-tools simg2img tool (#52)

--- test/fixtures/make_sparse.sh
+++ test/fixtures/make_sparse.sh
@@ -7,13 +7,10 @@ head -c 4096 </dev/zero > skip.bin
 
 cat raw.bin fill.bin skip.bin raw.bin raw.bin fill.bin fill.bin skip.bin skip.bin > test.bin
 
-img2sparse test.bin sparse.img
-
-# debug
-sparse2img -d sparse.img
+img2simg test.bin sparse.img
 
 # sanity check
-sparse2img sparse.img raw.img
+simg2img sparse.img raw.img
 cmp test.bin raw.img && echo "Success" || echo "Failure"
 
 rm raw.bin fill.bin skip.bin test.bin
