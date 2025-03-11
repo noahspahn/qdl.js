noahspahn/qdl.js

commit aa3aed500600ee076e42062fa3e5c21871eb9c4f

Reliably connect to device without replugging (#65)
- cli
  - [x] works after replugging
  - [x] works on 2nd, 3rd script run without replugging
- [x] works on demo

src/firehose.js
@@ -1,4 +1,4 @@
-import { concatUint8Array, containsBytes, compareStringToBytes, runWithTimeout, sleep } from "./utils"
+import { concatUint8Array, containsBytes, compareStringToBytes, runWithTimeout } from "./utils"
 import { toXml, xmlParser } from "./xml"
 
 
@@ -63,23 +63,13 @@ export class Firehose {
   async xmlSend(command, wait = true) {
     // FIXME: warn if command is shortened
     const dataToSend = new TextEncoder().encode(command).slice(0, this.cfg.MaxXMLSizeInBytes);
-    await this.cdc.write(dataToSend, wait);
-
-    let rData = new Uint8Array();
-    let counter = 0;
-    const timeout = 3;
-    while (!(containsBytes("<response value", rData))) {
-      const tmp = await this.cdc.read();
-      if (compareStringToBytes("", tmp)) {
-        counter += 1;
-        await sleep(50);
-        if (counter > timeout) {
-          break;
-        }
-      }
-      rData = concatUint8Array([rData, tmp]);
+    try {
+      await runWithTimeout(this.cdc.write(dataToSend, wait), 1000);
+    } catch (e) {
+      throw "Firehose - Timed out while sending command";
     }
 
+    const rData = await this.waitForData();
     const resp = this.xml.getResponse(rData);
     const status = !("value" in resp) || resp.value === "ACK" || resp.value === "true";
     if ("rawmode" in resp) {
@@ -113,9 +103,25 @@ export class Firehose {
       SkipStorageInit: this.cfg.SkipStorageInit,
       SkipWrite: this.cfg.SkipWrite,
     });
-    await this.xmlSend(connectCmd, false);
-    await sleep(80);
-    this.luns = Array.from({length: this.cfg.maxlun}, (x, i) => i);
+    await this.cdc.write(new TextEncoder().encode(connectCmd), false);
+    let data = await this.waitForData();
+    let response = this.xml.getResponse(data);
+    if (!("MemoryName" in response)) {
+      // not reached handler yet
+      data = await this.waitForData();
+      response = this.xml.getResponse(data);
+    }
+    if (response.value !== "ACK") {
+      throw new Error("Negative response");
+    }
+    const log = this.xml.getLog(data);
+    if (!log.find((message) => message.includes("Calling handler for configure"))) {
+      throw new Error("Failed to configure: handler not called");
+    }
+    if (!log.find((message) => message.includes("Storage type set to value UFS"))) {
+      throw new Error("Failed to configure: storage type not set");
+    }
+    this.luns = Array.from({ length: this.cfg.maxlun }, (x, i) => i);
     return true;
   }
 
@@ -160,21 +166,20 @@ export class Firehose {
   }
 
   /**
+   * @param {number} [retries]
    * @returns {Promise<Uint8Array>}
    */
-  async waitForData() {
+  async waitForData(retries = 3) {
     let tmp = new Uint8Array();
     let timeout = 0;
-
-    while (!containsBytes("response value", tmp)) {
-      const res = await this.cdc.read();
+    while (!containsBytes("<response", tmp)) {
+      const res = await runWithTimeout(this.cdc.read(), 150).catch(() => new Uint8Array());
       if (compareStringToBytes("", res)) {
         timeout += 1;
-        if (timeout === 4) {
-          break;
-        }
-        await sleep(20);
+        if (timeout > retries) break;
+        continue;
       }
+      timeout = 0;
       tmp = concatUint8Array([tmp, res]);
     }
     return tmp;

.github/workflows/hitl.yml
@@ -27,3 +27,4 @@ jobs:
           sleep 1
 
       - run: qdl.js
+      - run: qdl.js

src/qdl.js
@@ -2,7 +2,7 @@ import { Firehose } from "./firehose"
 import * as gpt from "./gpt"
 import { Sahara } from "./sahara";
 import * as Sparse from "./sparse";
-import { concatUint8Array, runWithTimeout, containsBytes } from "./utils"
+import { concatUint8Array, containsBytes } from "./utils"
 
 
 export class qdlDevice {
@@ -43,14 +43,18 @@ export class qdlDevice {
     if (!cdc.connected) throw new Error("Could not connect to device");
     console.debug("[qdl] QDL device detected");
     this.sahara = new Sahara(cdc, this.programmer);
-    if (!await runWithTimeout(this.sahara.connect(), 10000)) throw new Error("Could not connect to Sahara");
-    console.debug("[qdl] Connected to Sahara");
-    this.mode = "sahara";
-    await this.sahara.uploadLoader();
+    this.mode = await this.sahara.connect();
+    if (this.mode === "sahara") {
+      console.debug("[qdl] Connected to Sahara");
+      await this.sahara.uploadLoader();
+      this.mode = this.sahara.mode;
+    }
+    if (this.mode !== "firehose") {
+      throw new Error(`Unsupported mode: ${this.mode}. Please reboot the device.`);
+    }
     this.#firehose = new Firehose(cdc);
     if (!await this.firehose.configure()) throw new Error("Could not configure Firehose");
     console.debug("[qdl] Firehose configured");
-    this.mode = "firehose";
   }
 
   /**

src/sahara.js
@@ -1,5 +1,6 @@
 import { CommandHandler, cmd_t, sahara_mode_t, status_t, exec_cmd_t } from "./saharaDefs"
-import { concatUint8Array, packGenerator } from "./utils";
+import { containsBytes, packGenerator, runWithTimeout } from "./utils";
+import { toXml } from "./xml.js";
 
 
 export class Sahara {
@@ -17,18 +18,42 @@ export class Sahara {
   }
 
   /**
-   * TODO: detect other modes
-   * @returns {Promise<boolean>}
+   * @returns {Promise<string>}
    */
   async connect() {
-    const resp = await this.cdc.read(0xC * 0x4);
-    if (resp.length > 1 && resp[0] === 0x01) {
-      const pkt = this.ch.pkt_cmd_hdr(resp);
-      if (pkt.cmd === cmd_t.SAHARA_HELLO_REQ) {
-        return true;
+    let respPromise = this.cdc.read(0xC * 0x4);
+    let resp = await runWithTimeout(respPromise, 500).catch(() => new Uint8Array());
+    if (resp.length > 1) {
+      if (resp[0] === 0x01) {
+        const pkt = this.ch.pkt_cmd_hdr(resp);
+        if (pkt.cmd === cmd_t.SAHARA_HELLO_REQ) {
+          return "sahara";
+        }
+        if (pkt.cmd === cmd_t.SAHARA_END_TRANSFER) {
+          return "sahara";
+        }
+        throw "Sahara - Connect failed: unknown command";
+      }
+      if (containsBytes("<?xml", resp)) {
+        return "firehose";
+      }
+    } else {
+      try {
+        await runWithTimeout(this.cdc.write(new TextEncoder().encode(toXml("nop"))), 1000);
+        if (!resp) respPromise = this.cdc.read();
+        resp = await runWithTimeout(respPromise, 2000).catch(() => new Uint8Array());
+      } catch {
+        resp = new Uint8Array();
+      }
+      if (containsBytes("<?xml", resp)) {
+        return "firehose";
+      }
+      if (resp[0] === cmd_t.SAHARA_END_TRANSFER) {
+        return "sahara";
       }
     }
-    return false;
+    console.error("Device is in Sahara error state, please reboot the device.");
+    return "error";
   }
 
   async cmdHello(mode, version=2, version_min=1, max_cmd_len=0) {

src/utils.js
@@ -1,6 +1,3 @@
-export const sleep = ms => new Promise(r => setTimeout(r, ms));
-
-
 export class StructHelper {
   /**
    * @param {Uint8Array} data

commit 04bf80e29c18e906659ea025657aef63de1bdc02

fix(firehose): wait longer for slow responses (#80)
This doesn't affect the performance, it just allows the device more time
to respond. The erase command seems to be slower, for example.

Bug introduced in #65

src/firehose.js
@@ -173,7 +173,7 @@ export class Firehose {
     let tmp = new Uint8Array();
     let timeout = 0;
     while (!containsBytes("<response", tmp)) {
-      const res = await runWithTimeout(this.cdc.read(), 150).catch(() => new Uint8Array());
+      const res = await runWithTimeout(this.cdc.read(), 1000).catch(() => new Uint8Array());
       if (compareStringToBytes("", res)) {
         timeout += 1;
         if (timeout > retries) break;