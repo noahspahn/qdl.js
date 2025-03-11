f162bf0713b7ce846ed6764c7039c965a8b60a81
Cameron Clough <cameronjclough@gmail.com>
2025-03-11 13:46:25 +0000

    CLI: add commands (#77)
    Support extending CLI with different commands, pre-requisite for testing
    flashing in CI
    
    ```
    $ qdl.js
    Usage: qdl.js <command> [...flags] [...args]
    
    Commands:
      reset                                Reboot the device
      getactiveslot                        Get the active slot
      getstorageinfo                       Print UFS information
    
    Flags:
      --programmer <url>                   Use a different loader [default is comma 3/3X]
      -h, --help                           Display this menu and exit
    ```

--- src/bin/qdl.js
+++ src/bin/qdl.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env bun
+import arg from "arg";
 import { webusb } from "usb";
 
 import { qdlDevice } from "../qdl";
@@ -6,21 +7,51 @@ import { usbClass } from "../usblib";
 
 navigator.usb = webusb;
 
-const programmer = await fetch("https://raw.githubusercontent.com/commaai/flash/master/src/QDL/programmer.bin")
+const args = arg({
+  "--help": Boolean,
+  "-h": "--help",
+  "--programmer": String,
+});
+
+const { _: commands } = args;
+
+const help = `Usage: qdl.js <command> [...flags] [...args]
+
+Commands:
+  reset                                Reboot the device
+  getactiveslot                        Get the active slot
+  getstorageinfo                       Print UFS information
+
+Flags:
+  --programmer <url>                   Use a different loader [default is comma 3/3X]
+  -h, --help                           Display this menu and exit`;
+
+if (args["--help"] || commands.length === 0) {
+  console.info(help);
+  process.exit(0);
+}
+
+const programmerUrl = args["--programmer"] ?? "https://raw.githubusercontent.com/commaai/flash/master/src/QDL/programmer.bin";
+const programmer = await fetch(programmerUrl)
   .then((response) => response.blob())
   .then((blob) => blob.arrayBuffer());
 
 const qdl = new qdlDevice(programmer);
 await qdl.connect(new usbClass());
 
-if (Bun.argv.some((arg) => arg === "reset")) {
+if (commands[0] === "reset") {
   await qdl.reset();
-  process.exit(0);
+} else if (commands[0] === "getactiveslot") {
+  const activeSlot = await qdl.getActiveSlot();
+  console.info(activeSlot);
+} else if (commands[0] === "getstorageinfo") {
+  const storageInfo = await qdl.getStorageInfo();
+  storageInfo.serial_num = storageInfo.serial_num.toString(16).padStart(8, "0");
+  console.info(storageInfo);
+} else {
+  console.error(`Unrecognized command: ${commands[0]}`);
+  console.info(`\n${help}`)
+  process.exit(1);
 }
-
-const activeSlot = await qdl.getActiveSlot();
-console.debug("Active slot:", activeSlot);
-const storageInfo = await qdl.getStorageInfo();
-console.debug("UFS Serial Number:", storageInfo.serial_num.toString(16).padStart(8, "0"));
 
 process.exit(0);
